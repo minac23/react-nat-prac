b6088770e16b86d2e97c9c15a862b3c625fd9278
Celine Mina <ccmina23@gmail.com>
2021-12-26 21:17:17 -0800

    Workshop Assignment 2 Task 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -56,8 +56,16 @@ function RenderComments({comments}) {
     const renderCommentItem = ({item}) => {
         return (
             <View style={{margin: 10}}>
-                <Text style={{fontSize: 14}}>{item.text}</Text>
+                {/* <Text style={{fontSize: 14}}>{item.text}</Text>
                 <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text> */}
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Rating
+                    startingValue={item.rating}
+                    imageSize={10}
+                    style={{alignItems:'flex-start', paddingVertical: '5%'}}
+                    readonly
+                />
                 <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
             </View>
         );
@@ -80,7 +88,10 @@ class CampsiteInfo extends Component {
         super(props);
         this.state = {
             favorite: false,
-            showModal: false
+            showModal: false,
+            rating: 5,
+            author: '',
+            text:''
         };
     }
 
@@ -90,6 +101,21 @@ class CampsiteInfo extends Component {
 
     toggleModal() {
         this.setState({showModal: !this.state.showModal});
+    }
+
+    handleComment(campsiteId){
+        console.log(JSON.stringify(this.state));
+        this.toggleModal();
+    }
+
+    resetForm() {
+        this.setState({
+            favorite: false,
+            showModal: false,
+            rating: 5,
+            author: '',
+            text:''
+        });
     }
 
     static navigationOptions = {
@@ -116,6 +142,39 @@ class CampsiteInfo extends Component {
                     onRequestClose={() => this.toggleModal()}
                 >
                     <View style={styles.modal}>
+                        <Rating
+                            showRating
+                            startingValue={this.state.rating}
+                            imageSize={40}
+                            onFinishRating={rating=> this.setState({
+                                rating:rating
+                            })}
+                            style={{paddingVertical:10}}
+                        />
+                        <Input
+                            placeholder='Author'
+                            leftIcon={{type:'font-awesome', name:'user-o'}}
+                            leftIconContainerStyle={{paddingRight: 10}}
+                            onChangeText={value => this.setState({author: value})}
+                            //value
+                        />
+                        <Input
+                            placeholder='Comment'
+                            leftIcon={{type:'font-awesome', name:'comment-o'}}
+                            leftIconContainerStyle={{paddingRight: 10}}
+                            onChangeText={value => this.setState({text: value})}
+                            //value
+                        />
+                        <View style={{margin: 10}}>
+                            <Button
+                                onPress={() => {
+                                    this.handleComment(campsiteId);
+                                    this.resetForm();
+                                }}
+                                color='#5637DD'
+                                title='Submit'
+                            />
+                        </View>
                         <View style={{margin: 10}}>
                             <Button
                                 onPress={() => {
